f0ee1ec9a0db2b53e7f15ad82a4ee01086efabfe
wangyuan <wangy_60915950@163.com>
2022-02-17 23:24:51 +0800

    feat: 防抖事件

--- common-write/index.js
+++ common-write/index.js
@@ -1,3 +1,15 @@
-// 防抖
+// 防抖：事件在触发n秒后执行，如果n秒内再次触发，则重新计时
+function debounce(fn, delay = 1000) {
+  const timer = null
+  return (...args) => {
+    if(timer) {
+      clearTimeout(timer)
+      timer = null
+    }
+    timer = setTimeout(() => {
+      fn.call(this, args)
+    }, delay);
+  }
+}
 // 节流
 // 深拷贝
